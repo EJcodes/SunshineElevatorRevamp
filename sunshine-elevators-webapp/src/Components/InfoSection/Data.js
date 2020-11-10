12ee27ea5a282dc66b0e0d912e577371ae309dd3
EJcodes <IHMDemon.exe@gmail.com>
2020-11-09 21:03:36 -0500

    found some svgs for the page

---
+++ sunshine-elevators-webapp/src/Components/InfoSection/Data.js
@@ -0,0 +1,18 @@
+import { FaServicestack } from "react-icons/fa";
+
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: True,
+    lightTextDesc: true,
+    topLine: 'Services',
+    headline: 'blah blah blah',
+    description: 'Get that that hat when fat you cat',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('../../imgages/svg-1.svg'),
+    alt: 'Desc fo the image',
+    dark: true,
+    primary: true,
+    darkText: false
+}
