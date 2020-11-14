123b3af560ad1a5c05ef17c82427a9ab45fa393e
EJcodes <IHMDemon.exe@gmail.com>
2020-11-14 01:49:08 -0500

    footer in progress

--- sunshine-elevators-webapp/src/Components/Footer/index.js
+++ sunshine-elevators-webapp/src/Components/Footer/index.js
@@ -8,6 +8,8 @@ const Footer = () => {
                     <FooterLinksWrapper>
                         <FooterLinkTitle>
                             <FooterLink to="/signin">About us</FooterLink>
+                            <FooterLink to="/contactus">Contact us</FooterLink>
+                            <FooterLink to="/signin">Testimonials</FooterLink>
                         </FooterLinkTitle>
                     </FooterLinksWrapper>
                 </FooterLinksContainer>
