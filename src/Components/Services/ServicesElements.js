57001bc908af7163250adb5034e6392788c43ab6
Ejcodes <IHMDemon.exe@gmail.com>
2021-02-07 14:17:34 -0500

    fixing little details in the service cards in the service section

--- src/Components/Services/ServicesElements.js
+++ src/Components/Services/ServicesElements.js
@@ -48,7 +48,7 @@ export const ServicesCard = styled.div`
     padding: 30px;
     box-shadow: 0 1px 3px rgba(0,0,0,0.2);
     transition: all 0.2s ease-in-out;
-
+    min-height: 313px;
     &:hover {
         transform: scale(1.02);
         transition: all 0.2s ease-in-out;
